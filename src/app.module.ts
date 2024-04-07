c1d5683314610104d57d8e0856d2478d3c549856
Jose Cavazos <jgcavazos96@hotmail.com>
2024-04-06 18:54:26 -0600

    feat(db-connection): Implement Postgresql database with syncronization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './config/orm.config';
+import { ConfigModule } from '@nestjs/config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+    }),
+    UserModule,
+    TaskModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
